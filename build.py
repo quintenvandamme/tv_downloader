eff92c762e80c27de0fe32dc83b903f617e55fdb
quintenvandamme <quintenvandamme@duck.com>
2024-06-27 14:37:27 +0200

    fix

--- build.py
+++ build.py
@@ -116,10 +116,9 @@ def build():
         build_appimage()
     elif OS == "win32":
         _run_command('pyinstaller.exe -F --add-data "./out/ffmpeg.exe:./ffmpeg/" --add-data "./data/logo/logo-64x64.png:./data/logo/" --windowed --onefile --clean --name tvdownloader --icon=data/logo/logo-256x256.ico  gui/main.py')
-        _run_command('dir ./out/')
-        _run_command('dir ./dist/')
-        _run_command(f'move dist/tvdownloader.exe out/tvdownloader-{OS}-{ARCH}.exe')
-        _run_command('del dist/ build/ tvdownloader.spec ./out/ffmpeg.exe')   
+        _run_command(f'Move-Item -Path dist/tvdownloader.exe -Destination out/tvdownloader-{OS}-{ARCH}.exe')
+        _run_command('rmdir /s /q dist build tvdownloader-venv')  
+        _run_command('del tvdownloader.spec ./out/ffmpeg.exe') 
         _print_stage(f"Built out/tvdownloader-{OS}-{ARCH}")
 
     deactivate_venv()
